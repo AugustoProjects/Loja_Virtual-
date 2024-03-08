66e84488f197d98e078e15a79f323a2499e9f1b0
augus <augus@Netinho>
2024-03-08 17:20:03 -0300

    implementacão das classes model

---
+++ src/main/java/loja/model/pessoaJuridica.java
@@ -0,0 +1,61 @@
+package loja.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_juridica")
+@PrimaryKeyJoinColumn(name = "id")
+public class pessoaJuridica extends pessoa implements Serializable       {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	private String cnpj;
+	private String inscrEstadual; 
+	private String inscMunicipal; 
+	private String nomeFantasia; 
+	private String razaoSocial; 
+	private String categoria;
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getInscrEstadual() {
+		return inscrEstadual;
+	}
+	public void setInscrEstadual(String inscrEstadual) {
+		this.inscrEstadual = inscrEstadual;
+	}
+	public String getInscMunicipal() {
+		return inscMunicipal;
+	}
+	public void setInscMunicipal(String inscMunicipal) {
+		this.inscMunicipal = inscMunicipal;
+	}
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	} 
+	
+	
+}
