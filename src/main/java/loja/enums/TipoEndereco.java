c58c6f45cca9ccfbb63d9f0d3b18be084bf6e895
jose augusto <jose.auguston1010@gmail.com>
2024-03-15 03:16:27 -0300

    implementação das usuario e usuario acesso e enum de endereços

---
+++ src/main/java/loja/enums/TipoEndereco.java
@@ -0,0 +1,29 @@
+package loja.enums;
+
+public enum TipoEndereco {
+	
+	 COBRANCA("cobranca"),
+	 ENTREGA("entrega"); 
+	
+	private String descricao;
+
+	private TipoEndereco(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	
+
+
+	@Override
+	public String toString() {
+		
+		return this.descricao;
+	}
+
+}
